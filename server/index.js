c7d6bfba16a764ad82e95d8a4e6d606a27481248
Nathnael Desalegn <nathnaeldes@gmail.com>
2023-09-24 23:43:54 +0300

    edited

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-// Replace with your MongoDB connection string
 const dbUrl ="mongodb+srv://nathnaeldesalegn94:16381735@natdb.az1b1zx.mongodb.net/NatDb?retryWrites=true&w=majority";
 
-// Connect to MongoDB using Mongoose
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
@@ -18,7 +16,6 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Set up a simple route to check the status
 app.get('/check-mongoose-connection', (req, res) => {
   if (db.readyState === 1) {
     res.send('Mongoose is connected to MongoDB.');
@@ -27,7 +24,6 @@ app.get('/check-mongoose-connection', (req, res) => {
   }
 });
 
-// Start the Express server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
