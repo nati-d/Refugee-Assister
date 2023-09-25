f96b30c5090818d034a80a1f68c9d6102014a4cd
Nathnael Desalegn <nathnaeldes@gmail.com>
2023-09-25 08:01:53 +0300

    login

--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/authRoutes');
 app.use('/api/users', userRoutes);
+
+const authRoute = require('./routes/authRoutes');
+app.use('/auth', authRoute);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
