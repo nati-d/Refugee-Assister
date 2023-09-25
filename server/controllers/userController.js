cf9efa58d456bfc190f4f8dc4f13ae76c8f57f6c
Nathnael Desalegn <nathnaeldes@gmail.com>
2023-09-25 07:41:17 +0300

    edit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,45 @@
+// userController.js
 const db = require('../database');
 
 exports.signup = (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const {
+      username,
+      password,
+      phone_number,
+      full_name,
+      language,
+      educational_status,
+      interest,
+      location,
+      work_experience,
+    } = req.body;
 
-    const sql = 'INSERT INTO Users (username, email, password) VALUES (?, ?, ?)';
-    db.query(sql, [username, email, password], (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Registration failed' });
+    // Insert user into the database
+    const sql =
+      'INSERT INTO Users (username, password, phone_number, full_name, language, educational_status, interest, location, work_experience) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(
+      sql,
+      [
+        username,
+        password,
+        phone_number,
+        full_name,
+        language,
+        educational_status,
+        interest,
+        location,
+        work_experience,
+      ],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Registration failed' });
+        }
+        console.log('User registered:', result);
+        return res.status(201).json({ message: 'Registration successful' });
       }
-      console.log('User registered:', result);
-      return res.status(201).json({ message: 'Registration successful' });
-    });
+    );
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal server error' });
