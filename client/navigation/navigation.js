3cfd23ace674408c05d823839682f9d06474a863
Nathnael Desalegn <nathnaeldes@gmail.com>
2023-10-03 17:21:42 +0300

    authenticationCreated

--- client/navigation/navigation.js
+++ client/navigation/navigation.js
@@ -4,18 +4,41 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
-import SplashScreen from '../screens/SplashScreen'
 import OpeningScreen from '../screens/OpeningScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import useAuth from '../hooks/useAuth';
+import HomeScreen from '../screens/HomeScreen';
 
 function Navigation() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Opening" component={OpeningScreen}/>
-        <Stack.Screen name="Splash" component={SplashScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const { user } = useAuth();
+  if (user) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}>
+          {/*<Stack.Screen name="Opening" component={OpeningScreen}/>*/}
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}>
+          {/*<Stack.Screen name="Opening" component={OpeningScreen}/>*/}
+          {/*<Stack.Screen name="Splash" component={SplashScreen} />*/}
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 export default Navigation;
