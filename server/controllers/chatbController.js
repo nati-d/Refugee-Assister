f39602049b33fe6426ec52bd8c24712c4e900064
nati-d <74787412+nati-d@users.noreply.github.com>
2023-10-06 12:35:36 +0300

    Fix network error and modify chatbController.js

--- server/controllers/chatbController.js
+++ server/controllers/chatbController.js
@@ -1,12 +1,7 @@
-
-const { OpenAI } = require("openai");
+const axios = require('axios');
 
 const apiKey = process.env.API_KEY;
-const openai = new OpenAI({
-  apiKey,
-});
 
-// Create a conversation state variable to store previous messages
 let conversationState = [];
 
 // Format user input for the OpenAI API
@@ -22,27 +17,29 @@ function formatUserInput(userInput) {
 
 exports.chat = async (req, res) => {
   try {
-    const userInput = req.body.userInput;
+    const userInput = req.body.message;
     const messages = formatUserInput(userInput);
 
-    const response = await openai.chat.completions.create({
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
       model: "gpt-3.5-turbo",
       messages,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
     });
 
     // Extract the chatbot's response
-    const chatbotResponse = response.choices[0].message.content;
+    const chatbotResponse = response.data.choices[0].message.content;
 
     // Save the current conversation state for future reference
     conversationState = [...messages, { role: "assistant", content: chatbotResponse }];
 
-
-
     // Return the chatbot response as the API response
-    res.json({ response: chatbotResponse});
+    res.json({ response: chatbotResponse });
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({ error: "An error occurred" });
   }
-
 };
